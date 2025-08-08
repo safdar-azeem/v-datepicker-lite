817498e1bb35a1ccf5719d23662e2eb8ce86df7b
safdar-azeem <safdarazeem00@gmail.com>
2025-08-08 06:14:47 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,37 +5,37 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		vue(),
-		dts({
-			include: ['src/**/*.{ts,tsx,vue}', 'src/types/**/*.{ts,d.ts}'],
-			copyDtsFiles: true,
-		}),
-	],
-	resolve: {
-		alias: {
-			'@': '/src',
-		},
-	},
-	build: {
-		lib: {
-			entry: {
-				index: path.resolve(__dirname, 'src/index.ts'),
-			},
-			formats: ['es'],
-		},
+   plugins: [
+      vue(),
+      dts({
+         include: ['src/**/*.{ts,tsx,vue}', 'src/types/**/*.{ts,d.ts}'],
+         copyDtsFiles: true,
+      }),
+   ],
+   resolve: {
+      alias: {
+         '@': '/src',
+      },
+   },
+   build: {
+      lib: {
+         entry: {
+            index: path.resolve(__dirname, 'src/index.ts'),
+         },
+         formats: ['es'],
+      },
 
-		rollupOptions: {
-			input: {
-				index: path.resolve(__dirname, 'src/index.ts'),
-			},
-			external: ['vue'],
-			output: {
-				inlineDynamicImports: false,
-				globals: {
-					vue: 'Vue',
-				},
-			},
-		},
-	},
+      rollupOptions: {
+         input: {
+            index: path.resolve(__dirname, 'src/index.ts'),
+         },
+         external: ['vue'],
+         output: {
+            inlineDynamicImports: false,
+            globals: {
+               vue: 'Vue',
+            },
+         },
+      },
+   },
 })
