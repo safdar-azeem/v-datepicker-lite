cd7856a06885f0450af88ede604127c8af2e4351
safdar-azeem <safdarazeem00@gmail.com>
2025-08-08 06:14:47 +0500

    Update DatePickerHeader.vue

--- src/components/DatePickerHeader.vue
+++ src/components/DatePickerHeader.vue
@@ -2,101 +2,88 @@
 import { computed } from 'vue'
 import type { ViewMode } from '../types'
 import { datePickerMonthNames } from '../utils'
-import {
-	datePickerChevronLeft,
-	datePickerChevronRight,
-	datePickerCurrentDate,
-} from '../constants/icons'
+import { datePickerChevronLeft, datePickerChevronRight, datePickerCurrentDate } from '../constants/icons'
 
 interface Props {
-	currentDate: Date
-	viewMode: ViewMode
-	canGoPrev: boolean
-	canGoNext: boolean
+   currentDate: Date
+   viewMode: ViewMode
+   canGoPrev: boolean
+   canGoNext: boolean
 }
 
 interface Emits {
-	(e: 'prev'): void
-	(e: 'next'): void
-	(e: 'header-click'): void
-	(e: 'current-date'): void
+   (e: 'prev'): void
+   (e: 'next'): void
+   (e: 'header-click'): void
+   (e: 'current-date'): void
 }
 
 const props = defineProps<Props>()
 const emit = defineEmits<Emits>()
 
 const headerTitle = computed(() => {
-	const year = props.currentDate.getFullYear()
-	const month = props.currentDate.getMonth()
+   const year = props.currentDate.getFullYear()
+   const month = props.currentDate.getMonth()
 
-	switch (props.viewMode) {
-		case 'date':
-			return `${datePickerMonthNames[month]} ${year}`
-		case 'year':
-			const startYear = Math.floor(year / 10) * 10
-			const endYear = startYear + 9
-			return `${startYear}–${endYear}`
-		default:
-			return `${datePickerMonthNames[month]} ${year}`
-	}
+   switch (props.viewMode) {
+      case 'date':
+         return `${datePickerMonthNames[month]} ${year}`
+      case 'year':
+         const startYear = Math.floor(year / 10) * 10
+         const endYear = startYear + 9
+         return `${startYear}–${endYear}`
+      default:
+         return `${datePickerMonthNames[month]} ${year}`
+   }
 })
 
 const onPrev = () => {
-	emit('prev')
+   emit('prev')
 }
 
 const onNext = () => {
-	emit('next')
+   emit('next')
 }
 
 const onHeaderClick = () => {
-	emit('header-click')
+   emit('header-click')
 }
 
 const onCurrentDate = () => {
-	emit('current-date')
+   emit('current-date')
 }
 </script>
 
 <template>
-	<div class="date-picker-header">
-		<button
-			class="date-picker-title"
-			@click="onHeaderClick"
-			type="button">
-			{{ headerTitle }}
-		</button>
+   <div class="date-picker-header">
+      <button class="date-picker-title" @click="onHeaderClick" type="button">
+         {{ headerTitle }}
+      </button>
 
-		<button
-			class="date-picker-nav-button ml-auto"
-			:disabled="!canGoPrev"
-			@click="onPrev"
-			type="button"
-			aria-label="Previous">
-			<span
-				class="date-picker-nav-icon"
-				v-html="datePickerChevronLeft"></span>
-		</button>
+      <button
+         class="date-picker-nav-button ml-auto"
+         :disabled="!canGoPrev"
+         @click="onPrev"
+         type="button"
+         aria-label="Previous">
+         <span class="date-picker-nav-icon" v-html="datePickerChevronLeft"></span>
+      </button>
 
-		<button
-			class="date-picker-nav-button"
-			@click="onCurrentDate"
-			type="button"
-			aria-label="Current Date">
-			<span
-				class="date-picker-nav-icon"
-				v-html="datePickerCurrentDate"></span>
-		</button>
+      <button
+         class="date-picker-nav-button"
+         @click="onCurrentDate"
+         type="button"
+         aria-label="Current Date">
+         <span class="date-picker-nav-icon" v-html="datePickerCurrentDate"></span>
+      </button>
 
-		<button
-			class="date-picker-nav-button"
-			:disabled="!canGoNext"
-			@click="onNext"
-			type="button"
-			aria-label="Next">
-			<span
-				class="date-picker-nav-icon"
-				v-html="datePickerChevronRight"></span>
-		</button>
-	</div>
+      <button
+         class="date-picker-nav-button"
+         :disabled="!canGoNext"
+         @click="onNext"
+         type="button"
+         aria-label="Next">
+         <span class="date-picker-nav-icon" v-html="datePickerChevronRight"></span>
+      </button>
+   </div>
 </template>
